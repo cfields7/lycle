209463e6bb9c3dc2bcb49cdca93ddf83597e4a32
cfields7 <christopher.fields@me.com>
2024-02-04 05:35:41 -0500

    fix parakeet

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,42 @@
 import '../css/Game.css';
 import Character from './Character';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import Parakeet from '../data/Bird/parakeet.json';
 import StrayCat from '../data/Cat/stray.json';
 import Penguin from '../data/Penguin/normal.json';
 
-import Audio from '../audio/bird_fear.mp3';
+import BirdFear from '../audio/bird_fear.mp3';
+import BirdHappy from '../audio/bird_happy.mp3';
+import BirdSad from '../audio/bird_sad.mp3';
+import BirdMad from '../audio/bird_mad.mp3';
+
+import CatFear from '../audio/cat_fear.mp3';
+import CatHappy from '../audio/cat_happy.mp3';
+import CatSad from '../audio/cat_sad.mp3';
+import CatMad from '../audio/cat_mad.mp3';
+
+import PenguinFear from '../audio/penguin_fear.mp3';
+import PenguinHappy from '../audio/penguin_happy.mp3';
+import PenguinSad from '../audio/penguin_sad.mp3';
+import PenguinMad from '../audio/penguin_mad.mp3';
 
 function Game({ setCurrentPage, type, color, emotion, species, updateAnimalAttribute }) {
+  const BFRef = useRef(null);
+  const BHRef = useRef(null);
+  const BSRef = useRef(null);
+  const BMRef = useRef(null);
+
+  const CFRef = useRef(null);
+  const CHRef = useRef(null);
+  const CSRef = useRef(null);
+  const CMRef = useRef(null);
+
+  const PFRef = useRef(null);
+  const PHRef = useRef(null);
+  const PSRef = useRef(null);
+  const PMRef = useRef(null);
+
   let prefix;
   if (type === "Bird") {
     prefix = "PB"
@@ -54,9 +82,47 @@ function Game({ setCurrentPage, type, color, emotion, species, updateAnimalAttri
     if (nextId === prefix + "_MENU")
       setCurrentPage("Home");
     setId(nextId);
-    updateAnimalAttribute("emotion", getOptionById(type, nextId).emotion);
-    let audio = new Audio(Audio);
-    audio.play();
+
+    let emotion = getOptionById(type, nextId).emotion
+    updateAnimalAttribute("emotion", emotion);
+
+    switch(emotion) {
+      case("Fear"):
+        if (type === "Bird")
+          BFRef.current.play();
+        if (type === "Cat")
+          CFRef.current.play();
+        if (type === "Penguin")
+          PFRef.current.play();
+        break;
+      case("Happy"):
+      if (type === "Bird")
+        BHRef.current.play();
+      if (type === "Cat")
+        CHRef.current.play();
+      if (type === "Penguin")
+        PHRef.current.play();
+        break;
+      case("Sad"):
+      if (type === "Bird")
+        BSRef.current.play();
+      if (type === "Cat")
+        CSRef.current.play();
+      if (type === "Penguin")
+        PSRef.current.play();
+        break;
+      case("Mad"):
+        if (type === "Bird")
+          BMRef.current.play();
+        if (type === "Cat")
+          CMRef.current.play();
+        if (type === "Penguin")
+          PMRef.current.play();
+        break;
+
+        default:
+          break;
+    }
   }
 
   return(
@@ -71,6 +137,45 @@ function Game({ setCurrentPage, type, color, emotion, species, updateAnimalAttri
           <button onClick={() => advanceChoice(getOptionById(type, id).optionB)}>{getOptionById(type, getOptionById(type, id).optionB).option}</button>
         </div>
       </div>
+
+      <audio ref={BFRef}>
+        <source src={BirdFear} type="audio/mp3" />
+      </audio>
+      <audio ref={BHRef}>
+        <source src={BirdHappy} type="audio/mp3" />
+      </audio>
+      <audio ref={BMRef}>
+        <source src={BirdMad} type="audio/mp3" />
+      </audio>
+      <audio ref={BSRef}>
+        <source src={BirdSad} type="audio/mp3" />
+      </audio>
+
+      <audio ref={CFRef}>
+        <source src={CatFear} type="audio/mp3" />
+      </audio>
+      <audio ref={CHRef}>
+        <source src={CatHappy} type="audio/mp3" />
+      </audio>
+      <audio ref={CMRef}>
+        <source src={CatMad} type="audio/mp3" />
+      </audio>
+      <audio ref={CSRef}>
+        <source src={CatSad} type="audio/mp3" />
+      </audio>
+
+      <audio ref={PFRef}>
+        <source src={PenguinFear} type="audio/mp3" />
+      </audio>
+      <audio ref={PHRef}>
+        <source src={PenguinHappy} type="audio/mp3" />
+      </audio>
+      <audio ref={PMRef}>
+        <source src={PenguinMad} type="audio/mp3" />
+      </audio>
+      <audio ref={PSRef}>
+        <source src={PenguinSad} type="audio/mp3" />
+      </audio>
     </div>
   );
 }
