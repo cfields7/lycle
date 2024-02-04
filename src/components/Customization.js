cd1c6a1ae1f3fecc621d2b83b036a5471c9114c9
cfields7 <christopher.fields@me.com>
2024-02-04 06:59:38 -0500

    Selection of colors

--- src/components/Customization.js
+++ src/components/Customization.js
@@ -21,17 +21,19 @@ function Customization({ setCurrentPage, type, color, emotion, species, updateAn
         <div className="customizeTitle">Customize Your Character</div>
         <div className="colorLabel">Color</div>
         <div className="colorSelection">
-        <div className="color red" onClick={() => animalChangeColor("Red")}></div>
-        <div className="color orange" onClick={() => animalChangeColor("Orange")}></div>
-        <div className="color yellow" onClick={() => animalChangeColor("Yellow")}></div>
-        <div className="color green" onClick={() => animalChangeColor("Green")}></div>
-        <div className="color blue" onClick={() => animalChangeColor("Blue")}></div>
-        <div className="color white" onClick={() => animalChangeColor("White")}></div>
+        <div className={`color red ${color === "Red"}`} onClick={() => animalChangeColor("Red")}></div>
+        <div className={`color orange ${color === "Orange"}`} onClick={() => animalChangeColor("Orange")}></div>
+        <div className={`color yellow ${color === "Yellow"}`} onClick={() => animalChangeColor("Yellow")}></div>
+        <div className={`color green ${color === "Green"}`} onClick={() => animalChangeColor("Green")}></div>
+        <div className={`color blue ${color === "Blue"}`} onClick={() => animalChangeColor("Blue")}></div>
       </div>
         <div className="colorLabel">Special</div>
         <div className="colorSelection">
-          <div className="color july" onClick={() => animalChangeColor("July")}></div>
+          <div className={`color july ${color === "July"}`} onClick={() => animalChangeColor("July")}></div>
+          <div className={`color white ${color === "White"}`} onClick={() => animalChangeColor("White")}></div>
         </div>
+      </div>
+      <div className="startContainer">
         <button className="startButton" onClick={() => setCurrentPage("Game")}>Start Game!</button>
       </div>
 
