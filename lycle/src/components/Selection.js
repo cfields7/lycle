dcd3c2bbf45183f915276ffaef3353e62ca34a49
cfields7 <christopher.fields@me.com>
2024-02-03 21:43:05 -0500

    Character Selection Updates

--- lycle/src/components/Selection.js
+++ lycle/src/components/Selection.js
@@ -1,9 +1,17 @@
 import '../css/Selection.css';
+import Bird from '../images/Characters/Bird/Bird_Stencil.png';
+import Cat from '../images/Characters/Cat/Cat_Stencil.png';
+import Penguin from '../images/Characters/Penguin/Penguin_Stencil.png';
 
 function Selection({ setCurrentPage }) {
   return(
     <div className="Selection">
-      
+      <div className="characters">
+        <img className="animal" src={Bird} alt="Bird Selection"></img>
+        <img className="animal" src={Cat} alt="Cat Selection"></img>
+        <img className="animal" src={Penguin} alt="Penguin Selection"></img>
+      </div>
+      <div>Select an animal to live as</div>
     </div>
   );
 }
